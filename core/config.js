5837da59a646f5d709aa3690dce11f5426e31078
melvinsamuel070 <melvinsamuel070@gmail.com>
2025-01-14 12:18:52 +0100

    new

--- core/config.js
+++ core/config.js
@@ -2,29 +2,21 @@ require("dotenv").config();
 
 //Server Configuration
 const serverConfig = {
-    port: process.env.PORT,
-    hostname: process.env.HOSTNAME
-}
-
+  port: process.env.PORT,
+  hostname: process.env.HOSTNAME,
+};
 
 //Email Configuration
 const emailConfig = {
-    emailService: process.env.EMAIL_SERVICE,
-    emailHost: process.env.EMAIL_HOST,
-    emailPort: process.env.EMAIL_PORT,
-    emailIsSecure: process.env.EMAIL_IS_SECURE,
-    emailUsername: process.env.EMAIL_USER,
-    emailPassword: process.env.EMAIL_PASSWORD
-}
-
+  emailService: process.env.EMAIL_SERVICE,
+  emailHost: process.env.EMAIL_HOST,
+  emailPort: process.env.EMAIL_PORT,
+  emailIsSecure: process.env.EMAIL_IS_SECURE,
+  emailUsername: process.env.EMAIL_USER,
+  emailPassword: process.env.EMAIL_PASSWORD,
+};
 
 //Cors Configuration
-const corsConfig = {
+const corsConfig = {};
 
-}
-
-
-
-
-
-module.exports = { serverConfig, emailConfig, corsConfig }
+module.exports = { serverConfig, emailConfig, corsConfig };
--- core/dragonmailer.js
+++ core/dragonmailer.js
@@ -1,20 +1,15 @@
 const nodemailer = require("nodemailer");
-const { emailConfig } = require("./config")
-
+const { emailConfig } = require("./config");
 
 //transporter
 const dragonTransporter = nodemailer.createTransport({
-    host: emailConfig.emailHost,
-    port: emailConfig.emailPort,
-    secure: emailConfig.emailIsSecure,
-    auth: {
-        user: emailConfig.emailUsername,
-        pass: emailConfig.emailPassword
-    }
+  host: emailConfig.emailHost,
+  port: emailConfig.emailPort,
+  secure: emailConfig.emailIsSecure,
+  auth: {
+    user: emailConfig.emailUsername,
+    pass: emailConfig.emailPassword,
+  },
 });
 
-
 module.exports = dragonTransporter;
-
-
-
