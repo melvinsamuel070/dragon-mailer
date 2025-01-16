d15d94462042fb7e54d46646bd791d9e2f27d286
melvinsamuel070 <melvinsamuel070@gmail.com>
2025-01-16 12:37:59 +0100

    fix: correct validation in commit message

--- commitlint.config.js
+++ commitlint.config.js
@@ -2,12 +2,10 @@
 //   extends: ['@commitlint/config-conventional'],
 // }
 
-
 module.exports = {
-    extends: ['@commitlint/config-conventional'],
-    rules: {
-      'subject-empty': [0], // Disable subject validation
-      'type-empty': [0],    // Disable type validation
-    },
-  };
-  
+  extends: ['@commitlint/config-conventional'],
+  rules: {
+    'subject-empty': [0], // Disable subject validation
+    'type-empty': [0], // Disable type validation
+  },
+}
