a8b126c162e1e1a21b2e5f827188a62a8e3fd875
melvinsamuel070 <melvinsamuel070@gmail.com>
2025-01-20 17:42:01 +0100

    a simpl

--- commitlint.config.js
+++ commitlint.config.js
@@ -2,10 +2,10 @@
 //   extends: ['@commitlint/config-conventional'],
 // }
 
-// module.exports = {
-//   extends: ['@commitlint/config-conventional'],
-//   rules: {
-//     'type-empty': [0], // Allow commit messages without a type
-//     'subject-empty': [0], // Allow empty subjects if necessary
-//   },
-// }
+module.exports = {
+  extends: ['@commitlint/config-conventional'],
+  rules: {
+    'type-empty': [0], // Allow commit messages without a type
+    'subject-empty': [0], // Allow empty subjects if necessary
+  },
+}
