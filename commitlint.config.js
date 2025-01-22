2fb9b95f4e583396b126f2b1d9486cd88ad01e8e
melvinsamuel070 <melvinsamuel070@gmail.com>
2025-01-22 11:01:52 +0100

    Test Husky
            feat: integrate husky into the project

--- commitlint.config.js
+++ commitlint.config.js
@@ -2,10 +2,16 @@
 //   extends: ['@commitlint/config-conventional'],
 // }
 
+// module.exports = {
+//   extends: ['@commitlint/config-conventional'],
+//   rules: {
+//     'type-empty': [0], // Allow commit messages without a type
+//     'subject-empty': [0], // Allow empty subjects if necessary
+//   },
+// }
+
+
+// commitlint.config.js
 module.exports = {
   extends: ['@commitlint/config-conventional'],
-  rules: {
-    'type-empty': [0], // Allow commit messages without a type
-    'subject-empty': [0], // Allow empty subjects if necessary
-  },
-}
+};
